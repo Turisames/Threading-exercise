b25db6ed9a35d05ac8d8d8251d8719b7a13c3c8d
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-05-16 09:54:55 +0300

    Exercising on my own. Next up I'm going to look into POSIX mutexes.

--- Pthreads/main.cc
+++ Pthreads/main.cc
@@ -1,6 +1,32 @@
-#include "fromtutorialspoint.hh"
+//#include "fromtutorialspoint.hh"
 
+#include <iostream>
+#include <pthread.h>
 #include <QCoreApplication>
+
+static long universal;
+
+void * myOwnThreadFunction( void* Param )
+{
+    for (int i = 0; i < 101; ++i)
+    {
+        universal++;
+        std::cout << "Thread 1: The value of universal is " << universal << std::endl;
+    }
+
+    pthread_exit( NULL );
+}
+
+void * mySecondThreadFunction( void * Param  )
+{
+    for (int i = 0; i < 101; ++i)
+    {
+        universal--;
+        std::cout << "Thread 2: The value of universal is " << universal << std::endl;
+    }
+
+    pthread_exit( NULL );
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +34,19 @@ int main(int argc, char *argv[])
 
     //firstPart();
     //secondPart();
-    thirdPart();
+    //thirdPart();
+
+    universal = 0;
+    pthread_t thread1, thread2;
+    int param{ 100 };//(void*) &param
+
+    pthread_create( &thread1, NULL,
+                    myOwnThreadFunction,
+                    (void*) &param );
+    pthread_create( &thread2, NULL, mySecondThreadFunction, (void*) &param);
+
+    pthread_join( thread1, NULL );
+    pthread_join( thread2, NULL );
 
     return a.exec();
 }
