2cb3d71959fb6ea23d29424573a6b8946955ee96
Ville Nyrhilä <ville.nyrhila@student.tut.fi>
2016-05-16 11:54:17 +0300

    I believe I now understand how to use POSIX mutexes. Next I should look into condition variables.

--- Pthreads/main.cc
+++ Pthreads/main.cc
@@ -4,14 +4,17 @@
 #include <pthread.h>
 #include <QCoreApplication>
 
+static pthread_mutex_t GLOBAL_MUTEX = PTHREAD_MUTEX_INITIALIZER;
 static long universal;
 
 void * myOwnThreadFunction( void* Param )
 {
     for (int i = 0; i < 101; ++i)
     {
+        pthread_mutex_lock( &GLOBAL_MUTEX );
         universal++;
         std::cout << "Thread 1: The value of universal is " << universal << std::endl;
+        pthread_mutex_unlock( &GLOBAL_MUTEX );
     }
 
     pthread_exit( NULL );
@@ -21,8 +24,10 @@ void * mySecondThreadFunction( void * Param  )
 {
     for (int i = 0; i < 101; ++i)
     {
+        pthread_mutex_lock( &GLOBAL_MUTEX );
         universal--;
         std::cout << "Thread 2: The value of universal is " << universal << std::endl;
+        pthread_mutex_unlock( &GLOBAL_MUTEX );
     }
 
     pthread_exit( NULL );
@@ -40,13 +45,16 @@ int main(int argc, char *argv[])
     pthread_t thread1, thread2;
     int param{ 100 };//(void*) &param
 
-    pthread_create( &thread1, NULL,
-                    myOwnThreadFunction,
-                    (void*) &param );
+    pthread_create( &thread1, NULL, myOwnThreadFunction, (void*) &param );
     pthread_create( &thread2, NULL, mySecondThreadFunction, (void*) &param);
 
+    std::cout << "\tmain(): Done creating threads." << std::endl;
     pthread_join( thread1, NULL );
+    std::cout << "\tmain(): thread1 is ready." << std::endl;
     pthread_join( thread2, NULL );
+
+    std::cout << "\tmain(): Printing is ready." << std::endl;
+    pthread_exit( NULL );
 
     return a.exec();
 }
